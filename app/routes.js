560f4436ee20c00da193ee692d5b16ec572197fa
Emmanuel Agahiu <alcantara@Emmanuels-Air.lan>
2019-08-10 06:05:29 +0100

    fixed conflicts on route

--- app/routes.js
+++ app/routes.js
@@ -6,20 +6,8 @@ import AuthController from './controllers/auth.controller';
 import UsersController from './controllers/users.controller';
 import PostsController from './controllers/posts.controller';
 import ProjectController from './controllers/projects.controller';
-<<<<<<< HEAD
-import WalletController from './controllers/wallet.controller';
 import Admincontroller from './controllers/admin.controller';
 
-=======
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public')
-    },
-    filename:  (req, file, cb) => {
-      cb(null,    Date.now() + '-'+ file.originalname )
-    }
-  })
->>>>>>> 4142cd3ca6919961809d832d014c2fb2882c03b9
 import multer from 'multer';
 const upload = multer({storage:storage});
 import authenticate from './middleware/authenticate';
