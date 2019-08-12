f124dbb0eaf35a7fc0062b6cab7203f846d545c4
Emmanuel Agahiu <alcantara@Emmanuels-Air.lan>
2019-08-12 23:45:45 +0100

    adjusted the routes

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/uploads')
+  destination: function(req, file, cb) {
+    cb(null, './public/uploads');
   },
-  filename:  (req, file, cb) => {
-    cb(null,    Date.now() + '-'+ file.originalname )
-  }
-})
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
 import MetaController from './controllers/meta.controller';
 import AuthController from './controllers/auth.controller';
 import UsersController from './controllers/users.controller';
@@ -16,15 +16,13 @@ import ProjectController from './controllers/projects.controller';
 import Admincontroller from './controllers/admin.controller';
 
 import multer from 'multer';
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 import authenticate from './middleware/authenticate';
 import accessControl from './middleware/access-control';
 import errorHandler from './middleware/error-handler';
 import walletController from './controllers/wallet.controller';
-import Transaction from './controllers/transaction.controller'
+import Transaction from './controllers/transaction.controller';
 import kycController from './controllers/kyc.controller';
-
-
 
 const routes = new Router();
 
@@ -34,25 +32,27 @@ routes.get('/', authenticate, MetaController.index);
 routes.post('/auth/login', AuthController.login);
 routes.post('/auth/forgotpassword', AuthController.forgotPassword);
 routes.get('/auth/resetpassword/:token', AuthController.resetPassword);
-routes.post('/auth/register',AuthController.create);
-
+routes.post('/auth/register', AuthController.create);
 
 // Users
-//routes.get('/user/dashboard',authenticate, UsersController.dashboard);
+// routes.get('/user/dashboard',authenticate, UsersController.dashboard);
 routes.get('/users', UsersController.search);
 routes.get('/users/me', authenticate, UsersController.fetch);
 routes.put('/users/me', authenticate, UsersController.update);
 routes.delete('/users/me', authenticate, UsersController.delete);
-routes.get('/users/:username', UsersController._populate, UsersController.fetch);
+routes.get(
+  '/users/:username',
+  UsersController._populate,
+  UsersController.fetch
+);
 routes.post('/users', UsersController.create);
 
-
-//Wallet Routes
+// Wallet Routes
 routes.get('/wallet', authenticate, walletController.getWallet);
 routes.post('/wallet/fund', authenticate, walletController.fundWallet);
 routes.post('/wallet/withdraw', authenticate, walletController.withdraw);
 
-//Wallet transactions
+// Wallet transactions
 routes.get('/transactions', authenticate, Transaction.all);
 routes.get('/transactions/user', authenticate, Transaction.self);
 routes.get('/transactions/:id', authenticate, Transaction.one);
@@ -60,11 +60,19 @@ routes.get('/transactions/:id', authenticate, Transaction.one);
 // projects
 routes.get('/projects', ProjectController.search);
 routes.post('/projects', authenticate, ProjectController.create);
-routes.get('/projects/:id', ProjectController._populate, ProjectController.fetch);
+routes.get(
+  '/projects/:id',
+  ProjectController._populate,
+  ProjectController.fetch
+);
 routes.delete('/projects/:id', authenticate, ProjectController.delete);
 
-//kyc
-routes.post('/users/kyc', [authenticate, upload.single('file')], kycController.create);
+// kyc
+routes.post(
+  '/users/kyc',
+  [authenticate, upload.single('file')],
+  kycController.create
+);
 
 // Post
 routes.get('/posts', PostsController.search);
@@ -75,10 +83,10 @@ routes.delete('/posts/:id', authenticate, PostsController.delete);
 // Admin
 routes.get('/admin', accessControl('admin'), MetaController.index);
 routes.get('/admin/users', accessControl('customer'), Admincontroller.users);
-routes.get('/admin/user/:id',accessControl('customer'), Admincontroller.user);
-routes.put('/admin/kyc',authenticate, Admincontroller.updateKycRequest);
+routes.get('/admin/user/:id', accessControl('customer'), Admincontroller.user);
+routes.put('/admin/kyc', authenticate, Admincontroller.updateKycRequest);
 routes.get('/admin/kyc/:id', Admincontroller.shuptiproverifcation);
-routes.get('/admin/kyc', authenticate,Admincontroller.getPendingKycRequest);
+routes.get('/admin/kyc', authenticate, Admincontroller.getPendingKycRequest);
 
 routes.use(errorHandler);
 
